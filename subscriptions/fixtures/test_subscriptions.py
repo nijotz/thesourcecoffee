eae0b381faaed66a2d05accbc05bd22b974edf10
Nick Tzaperas <nick@nijotz.com>
2013-03-11 16:02:16 -0700

    Fixed subscription fixtures so they can be run repeatedly

--- subscriptions/fixtures/test_subscriptions.py
+++ subscriptions/fixtures/test_subscriptions.py
@@ -1,15 +1,24 @@
-from mezzanine.pages.models import Page
 from products.models import Product
 from subscriptions.models import Subscription, SubscriptionType
 
-SubscriptionType.objects.create(amount=4, period=14)
-SubscriptionType.objects.create(amount=8, period=14)
-SubscriptionType.objects.create(amount=12, period=14)
-SubscriptionType.objects.create(amount=20, period=14)
-SubscriptionType.objects.create(amount=50, period=14)
+for amount in (4, 8, 12, 20, 50):
+    try:
+        SubscriptionType.objects.filter(amount=amount, period=14).delete()
+    except SubscriptionType.DoesNotExist:
+        pass
+
+    SubscriptionType.objects.create(amount=amount, period=14)
 
 for product in Product.objects.all():
     for sub_type in SubscriptionType.objects.all():
+        try:
+            Subscription.objects.filter(
+                subscription_type=sub_type,
+                product=product
+            ).delete()
+        except Subscription.DoesNotExist:
+            pass
+
         Subscription.objects.create(
             subscription_type=sub_type,
             product=product,
