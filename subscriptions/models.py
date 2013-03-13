95bd83f53e23f93b95ff0f3d1d43d8ecb15b2d0f
Nick Tzaperas <nick@nijotz.com>
2013-03-13 15:05:35 -0700

    Removed custom amount from CustomerSubscription (custom amounts will be new Subscriptions)

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -4,9 +4,10 @@ from base.models import Customer
 
 class Subscription(models.Model):
     amount = models.FloatField()
+    active = models.BooleanField()
+    public = models.BooleanField()
 
 
 class CustomerSubscription(models.Model):
     customer = models.ForeignKey(Customer)
     subscription = models.ForeignKey(Subscription)
-    custom_amount = models.IntegerField()
