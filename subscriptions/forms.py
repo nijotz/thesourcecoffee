08d8f1346a2975fbe1ab43c53949cffa71aa4c76
Nick Tzaperas <nick@nijotz.com>
2013-03-16 22:19:03 -0700

    Filter locations in subscription form by customer's area

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db.models import Q
+from locations.models import Location
 from subscriptions.models import CustomerSubscription, Subscription
 
 class CustomerSubscriptionForm(forms.ModelForm):
@@ -7,11 +8,16 @@ class CustomerSubscriptionForm(forms.ModelForm):
     #    queryset=Subscription.objects.filter(active=True, public=True))
 
     def __init__(self, customer, *args, **kwargs):
+
         super(CustomerSubscriptionForm, self).__init__(*args, **kwargs)
-        queryset = Subscription.objects.filter(
+
+        sub_qs = Subscription.objects.filter(
             Q(customer_subscriptions__customer=customer) |
             Q(public=True) )
-        self.fields['subscription'].queryset = queryset
+        self.fields['subscription'].queryset = sub_qs
+
+        loc_qs = Location.objects.filter(area=customer.area)
+        self.fields['location'].queryset = loc_qs
 
     class Meta:
         model = CustomerSubscription
