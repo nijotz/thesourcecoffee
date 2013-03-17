0f618941a4fb4ba2220c792dff486afe3ff4e2f2
Nick Tzaperas <nick@nijotz.com>
2013-03-16 17:20:47 -0700

    Added south support to the customers model module

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django_localflavor_us.models import PhoneNumberField
+from south.modelsinspector import add_introspection_rules
+
+
+# South needs to know about the PhoneNumberField
+add_introspection_rules([], ["^django_localflavor_us\.models\.PhoneNumberField"])
+
 
 class Customer(models.Model):
     user = models.OneToOneField(User)
