4cd7d3dc392967c4e16ae3c28374b983a201c3db
Nick Tzaperas <nick@nijotz.com>
2013-03-26 14:00:51 -0700

    Updated live settings.  They broke cookies

--- deploy/live_settings.py
+++ deploy/live_settings.py
@@ -16,17 +16,17 @@ DATABASES = {
     }
 }
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTOCOL", "https")
-
-CACHE_MIDDLEWARE_SECONDS = 60
-
-CACHE_MIDDLEWARE_KEY_PREFIX = "%(proj_name)s"
-
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
-        "LOCATION": "127.0.0.1:11211",
-    }
-}
-
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTOCOL", "https")
+#
+#CACHE_MIDDLEWARE_SECONDS = 60
+#
+#CACHE_MIDDLEWARE_KEY_PREFIX = "%(proj_name)s"
+#
+#CACHES = {
+#    "default": {
+#        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+#        "LOCATION": "127.0.0.1:11211",
+#    }
+#}
+#
+#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
