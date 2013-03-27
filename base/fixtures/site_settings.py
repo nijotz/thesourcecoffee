f3d5bf4e690e683cd97ba74128eb5984b045359b
Nick Tzaperas <nick@nijotz.com>
2013-03-27 16:18:14 -0700

    Fixed up site settings

--- base/fixtures/site_settings.py
+++ base/fixtures/site_settings.py
@@ -1,5 +1,6 @@
 from base.models import SiteSetting
 
+SiteSetting.objects.all().delete()
 SiteSetting.objects.create(
     key='subscription.length',
     description='Number of days a subscription lasts',
--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SiteSetting(models.Model):
     value = JSONField()
-    key = models.CharField(max_length=256)
+    key = models.CharField(max_length=256, unique=True)
     description = models.CharField(max_length=1024)
 
     def __unicode__(self):
