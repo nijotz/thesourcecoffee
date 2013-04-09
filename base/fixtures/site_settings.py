ec2597af37c3114febd77bbe99135c5decdbd359
Nick Tzaperas <nick@nijotz.com>
2013-04-09 00:32:08 -0700

    Added a setting for number of orders in a subscription

--- base/fixtures/site_settings.py
+++ base/fixtures/site_settings.py
@@ -11,3 +11,8 @@ SiteSetting.objects.create(
     key='locations.capacity',
     description='Number of pounds a location can store',
     value=100)
+
+SiteSetting.objects.create(
+    key='subscriptions.number_of_orders',
+    description='Number of orders received per subscription period',
+    value=12)
