820912735178b65f9919a11c355ee18b4dbdf1ff
Nick Tzaperas <nick@nijotz.com>
2013-04-11 17:11:33 -0700

    Move test_signup to setUp, so other tests can use the new user

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -12,16 +12,18 @@ from subscriptions.models import Plan, Subscription, LocationFullException
 
 
 class SubscriptionTestCase(TestCase):
-    fixtures = ['base/fixtures/site_settings.json']
+    fixtures = [
+        'base/fixtures/site_settings.json',
+        'subscriptions/fixtures/subscriptions.json',]
 
 
     def setUp(self):
+
         super(SubscriptionTestCase, self).setUp()
 
-        self.area = Area.objects.create(name='Test Area')
-        self.plan = Plan.objects.create(amount=3, price=20.00)
-        self.location = Location.objects.create(area=self.area, address='Test')
+        self.plan = Plan.objects.all()[0]
 
+        # Sign up a user to be used for other tests
         post = {
             'first_name': 'Test',
             'last_name': 'User',
@@ -36,8 +38,6 @@ class SubscriptionTestCase(TestCase):
             'code': '12345',
             'plan': self.plan.id,
         }
-        profile_form = get_profile_form()
-        self.customer = profile_form(post).save()
 
         token = stripe.Token.create(
             card={
@@ -46,7 +46,14 @@ class SubscriptionTestCase(TestCase):
                 'exp_year': datetime.now().year + 1,
                 'cvc': '123',
             })
-        self.customer.update_card(token.id)
+        post['stripeToken'] = token.id
+
+        client = Client()
+        response = client.post(reverse('signup'), post)
+        self.assertEqual(response.status_code, 302)
+
+        self.customer = Customer.objects.all()[0]
+        self.customer.password_text = post['password1']
 
 
     def test_location_capacity(self):
@@ -68,8 +75,7 @@ class SubscriptionTestCase(TestCase):
 
 
     def test_orders_are_created(self):
-        sub = Subscription(plan=self.plan, customer=self.customer)
-        sub.save()
+        sub = self.customer.subscription
         self.assertTrue(len(Order.objects.filter(subscription=sub)) > 0)
 
 
@@ -112,31 +118,13 @@ class SubscriptionTestCase(TestCase):
         self.assertEqual(required, True)
 
 
-    def test_signup(self):
-        post = {
-            'first_name': 'Test',
-            'last_name': 'User',
-            'username': 'TestUser1',
-            'email': 'testuser1@example.com',
-            'password1': 'testpass',
-            'password2': 'testpass',
-            'phone': '555-555-5555',
-            'street': '123 Easy St',
-            'city': 'City',
-            'state': 'TX',
-            'code': '12345',
-            'plan': self.plan.id,
-        }
-
-        token = stripe.Token.create(
-            card={
-                'number': '4242424242424242',
-                'exp_month': '12',
-                'exp_year': datetime.now().year + 1,
-                'cvc': '123',
-            })
-        post['stripeToken'] = token.id
-
+    def test_order_list(self):
         client = Client()
-        response = client.post(reverse('signup'), post)
-        self.assertEqual(response.status_code, 302)
+        post = {
+            'username': self.customer.user.username,
+            'password': self.customer.password_text
+        }
+        client.post(reverse('login'), post)
+        response = client.get(reverse('orders_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.context['orders']) > 0)
