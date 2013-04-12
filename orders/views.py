35144e22aa6fcb5ce064e3dcf010d463330c445e
Nick Tzaperas <nick@nijotz.com>
2013-04-11 17:10:55 -0700

    Only one type of order now, no need for select_subclasses

--- orders/views.py
+++ orders/views.py
@@ -4,5 +4,5 @@ from django.contrib.auth.decorators import login_required
 @login_required
 @render_to('orders/list.html')
 def list(request):
-    orders = request.user.customer.orders.all().select_subclasses()
+    orders = request.user.customer.orders.all()
     return locals()
