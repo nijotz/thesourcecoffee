c8994ffe31428cc3c186b65f3764f1d7c4ce7ad7
Nick Tzaperas <nick@nijotz.com>
2013-04-16 00:01:27 -0700

    Added mailing sticker PDF

--- base/admin.py
+++ base/admin.py
@@ -10,10 +10,15 @@ def get_admin_site(BaseAdminSite):
 
             urlpatterns = super(AdminSite, self).get_urls(*args, **kwargs)
             urlpatterns += patterns('',
-                url(r'^orders/fulfillment/',
+                url(r'^orders/fulfillment/$',
                     self.admin_view(orders.views.fulfillment),
-                    name='admin_orders_fulfillment')
-                )
+                    name='admin_orders_fulfillment',
+                ),
+                url(r'^orders/fulfillment/mailing_stickers',
+                    self.admin_view(orders.views.mailing_stickers),
+                    name='admin_orders_mailing_stickers',
+                ),
+            )
             return urlpatterns
 
     return AdminSite()
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,11 @@
 from datetime import date
+from io import BytesIO
 from annoying.decorators import render_to
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponse
+from django.shortcuts import render
+import xhtml2pdf.pisa
+from customers.models import Customer
 from orders.models import Order
 
 
@@ -17,3 +22,24 @@ def list(request):
 def fulfillment(request):
     orders = Order.objects.filter(to_be_fulfilled__lte=date.today())
     return locals()
+
+
+@user_passes_test(lambda u: u.is_superuser)
+@login_required
+def mailing_stickers(request):
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mailingstickers.pdf"'
+
+    buffer = BytesIO()
+
+    orders = Order.objects.filter(to_be_fulfilled__lte=date.today()).select_related('customer')
+    customers = [ o.customer for o in orders ]
+
+    html = render(request, 'orders/mailing_stickers.html', locals())
+    xhtml2pdf.pisa.CreatePDF(html.content, buffer)
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
