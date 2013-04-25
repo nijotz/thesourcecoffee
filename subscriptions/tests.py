6c98687d9d8a967b3f37109d9fcaef0784ba3602
Nick Tzaperas <nick@nijotz.com>
2013-04-25 11:51:28 -0700

    Fixed subscription test

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -1,4 +1,4 @@
-from customers.tests import CustomerTestCase
+from customers.tests import CustomerTestCase, signup_test_customer
 
 class SubscriptionTestCase(CustomerTestCase):
 
@@ -11,9 +11,8 @@ class SubscriptionTestCase(CustomerTestCase):
 
         post = {
             'plan': None,
-            'email': 'testuser1@example.com'
         }
-        (response, post) = self.base_sign_up(post)
+        (response, post) = signup_test_customer(post)
 
         errors = response.context['subscription_form'].errors
         self.assertIn('plan', errors.keys())
