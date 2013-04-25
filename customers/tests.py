0cbd0502af052def44d7c313bdc8a73d9a757326
Nick Tzaperas <nick@nijotz.com>
2013-04-25 11:36:04 -0700

    Moved test customer sign up code to customer models

--- customers/tests.py
+++ customers/tests.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 import stripe
-from customers.models import Customer
+from customers.models import Customer, signup_test_customer
 from subscriptions.models import Plan
 
 
@@ -11,47 +11,11 @@ class CustomerTestCase(TestCase):
         'base/fixtures/site_settings.json',
         'subscriptions/fixtures/subscriptions.json',]
 
-    def base_sign_up(self, custom_post={}):
-        self.plan = Plan.objects.all()[0]
-
-
-        # Sign up a user to be used for other tests
-        post = {
-            'first_name': 'Test',
-            'last_name': 'User',
-            'username': 'TestUser',
-            'email': 'testuser@example.com',
-            'password1': 'testpass',
-            'password2': 'testpass',
-            'phone': '555-555-5555',
-            'street': '123 Easy St',
-            'city': 'City',
-            'state': 'TX',
-            'code': '12345',
-            'plan': self.plan.id,
-        }
-        post.update(custom_post)
-
-        token = stripe.Token.create(
-            card={
-                'number': '4242424242424242',
-                'exp_month': '12',
-                'exp_year': datetime.now().year + 1,
-                'cvc': '123',
-            })
-        post['stripeToken'] = token.id
-
-        client = Client()
-        response = client.post(reverse('signup'), post)
-
-        return (response, post)
-
-
-    def setUp(self, custom_post={}):
+    def setUp(self, custom_data={}):
 
         super(CustomerTestCase, self).setUp()
 
-        (response, post) = self.base_sign_up(custom_post)
+        (response, post) = signup_test_customer(custom_data)
 
         self.customer = Customer.objects.get(user__email=post['email'])
         self.customer.password_text = post['password1']
