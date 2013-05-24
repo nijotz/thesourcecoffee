39646013308176e1e2307ce060a2563958ea70ba
Nick Tzaperas <nick@nijotz.com>
2013-05-23 17:04:27 -0700

    Fixed customer signup

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^signup$', 'customers.views.signup', name="signup"),
+    url(r'^signup/$', 'customers.views.signup', name="signup"),
     url(r'^$', 'customers.views.home', name="account_home"),
 )
