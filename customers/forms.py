134fa2646d715b33896bbebc066c5e9e4b4ac70a
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-02 21:33:43 -0700

    Whitespace. Minor.

--- customers/forms.py
+++ customers/forms.py
@@ -6,7 +6,7 @@ class CustomerForm(ProfileForm):
     def __init__(self, *args, **kwargs):
         super(CustomerForm, self).__init__(*args, **kwargs)
         self.fields['zipcode'] = forms.USZipCodeField()
-    
+
     def save(self, *args, **kwargs):
         user = super(CustomerForm, self).save(*args, **kwargs)
         return user
