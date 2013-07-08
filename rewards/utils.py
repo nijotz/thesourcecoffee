598e2d05ed4765d4369f67dd6248bdac1e590db7
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-07 21:25:19 -0700

    First go at the email stuff, mostly done. Somebody needs to go over the email content to make sure it's all correct.

--- rewards/utils.py
+++ rewards/utils.py
@@ -1,8 +1,10 @@
 from base.models import SiteSetting
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.core.mail import EmailMultiAlternatives
 from django.core.urlresolvers import reverse
 from django.template import Context, loader
+
 HASH_LETTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def base62encode(num_to_encode):
@@ -18,14 +20,25 @@ def base62encode(num_to_encode):
 def send_invite_email(inviter, email_address):
     signup_url = reverse("signup")
     current_site = Site.objects.get(name="Default").domain
+    code = inviter.invite_code()
     invite_url = "https://{current_site}{signup_url}?code={code}".format(
         current_site=current_site,
         signup_url=signup_url,
-        code="BLABLABLA")
+        code=code)
     context = Context({
         "STATIC_URL": settings.STATIC_URL,
         "current_site": current_site,
         "invite_url": invite_url
     })
+
     invite_email_subject = SiteSetting.objects.get(key="rewards.invite_email_subject")
+
+    rendered_html_template = loader.render_to_string("rewards/invite_email.html", context_instance=context)
+    rendered_txt_template = loader.render_to_string("rewards/invite_email.txt", context_instance=context)
+    email = EmailMultiAlternatives(invite_email_subject,
+        rendered_txt_template,
+        "The Source Coffee <noreply@thesourcecoffee.com>",
+        [email_address])
+    email.attach_alternative(rendered_html_template, "text/html")
+    email.send(fail_silently=False)
 
