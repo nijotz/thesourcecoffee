4148841080ab63cc62869d1116bfe242cae7f5f1
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-07 21:10:06 -0700

    Working on the email stuff, add a version 0. Somebody should probably go through it and change the text for it. Also added a debug view so you can look at the email in a browser.

--- rewards/utils.py
+++ rewards/utils.py
@@ -1,3 +1,8 @@
+from base.models import SiteSetting
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
+from django.template import Context, loader
 HASH_LETTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def base62encode(num_to_encode):
@@ -9,3 +14,18 @@ def base62encode(num_to_encode):
         code.append(remainder)
 
     return reduce(lambda accum, num: accum + HASH_LETTERS[num], code, "")
+
+def send_invite_email(inviter, email_address):
+    signup_url = reverse("signup")
+    current_site = Site.objects.get(name="Default").domain
+    invite_url = "https://{current_site}{signup_url}?code={code}".format(
+        current_site=current_site,
+        signup_url=signup_url,
+        code="BLABLABLA")
+    context = Context({
+        "STATIC_URL": settings.STATIC_URL,
+        "current_site": current_site,
+        "invite_url": invite_url
+    })
+    invite_email_subject = SiteSetting.objects.get(key="rewards.invite_email_subject")
+
