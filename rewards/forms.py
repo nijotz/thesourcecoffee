e5626d23c9e7ec27594fff5b9a01e2df7c586d96
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-07 20:20:46 -0700

    Finished form stuff for the "Invite your friends" email stuff on the rewards page.

--- rewards/forms.py
+++ rewards/forms.py
@@ -1,5 +1,24 @@
 from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from mezzanine.accounts.forms import ProfileForm
+
+class EmailListForm(forms.Form):
+    list_of_emails = forms.CharField(widget=forms.Textarea(attrs={"class": "shadow"}),
+        label = "You can also enter friends' email addresses below, separated by a comma to invite them.")
+
+    def clean_list_of_emails(self):
+        emails = self.cleaned_data["list_of_emails"]
+        emails = emails.replace(" ", "")
+        emails = emails.split(",")
+
+        for email in emails:
+            try:
+                validate_email(email)
+            except ValidationError, e:
+                raise ValidationError("Not all emails were valid.")
+
+        self.cleaned_data["list_of_emails"] = emails
 
 class RewardCodeForm(forms.Form):
     invite_code = forms.CharField(max_length=10,
--- rewards/views.py
+++ rewards/views.py
@@ -1,7 +1,13 @@
 from annoying.decorators import render_to
 from django.contrib.auth.decorators import login_required
+from rewards.forms import EmailListForm
 
 @login_required
 @render_to('rewards/home.html')
 def home(request):
+    email_list_form = EmailListForm(request.POST or None)
+    if request.method == 'POST':
+        if email_list_form.is_valid():
+            email_addresses = email_list_form.cleaned_data["list_of_emails"]
+            email_success = True
     return locals()
