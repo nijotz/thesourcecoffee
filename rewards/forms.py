205fbffb7d9f6c95224f0a12ae70bc99ac686113
Nick Tzaperas <nick@nijotz.com>
2013-07-21 14:53:39 -0700

    Added code to the RewardCodeForm to not require the code and check that code is valid

--- rewards/forms.py
+++ rewards/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from mezzanine.accounts.forms import ProfileForm
+from rewards.models import InviteCode
 
 class EmailListForm(forms.Form):
     list_of_emails = forms.CharField(widget=forms.Textarea(attrs={"class": "shadow"}),
@@ -22,5 +23,17 @@ class EmailListForm(forms.Form):
 
 class RewardCodeForm(forms.Form):
     invite_code = forms.CharField(max_length=10,
-        label = "Have a reward code?")
+        label = "Have a reward code?", required=False)
 
+    def clean_invite_code(self):
+
+        code = self.cleaned_data['invite_code']
+
+        if code == '':
+            return code
+
+        try:
+            InviteCode.objects.get(code=code)
+            return code
+        except:
+            raise forms.ValidationError("Invalid code")
