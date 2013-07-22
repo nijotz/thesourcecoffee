9b78b6de6eb0cd7f8664d58f30315b8f7afe62bb
Nick Tzaperas <nick@nijotz.com>
2013-07-21 21:43:08 -0700

    Added labels to gift/recipient fields on gift form

--- gifts/forms.py
+++ gifts/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from gifts.models import GiftSubscription
 
 class GiftSubscriptionForm(forms.Form):
-    gifter = forms.EmailField()
-    giftee = forms.EmailField()
+    gifter = forms.EmailField(label="Your e-mail")
+    giftee = forms.EmailField(label="Recipient's e-mail")
